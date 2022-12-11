ba0d3f02bacc279c4b062c1ece1b1b6fc3897a81
Gabriel Boucher <gab.boucher91@gmail.com>
2022-12-11 17:04:05 -0500

    Created the modules for the routing

---
+++ src/app/campgrounds/campgrounds.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CampgroundsRoutingModule } from './campgrounds-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CampgroundsRoutingModule
+  ]
+})
+export class CampgroundsModule { }
