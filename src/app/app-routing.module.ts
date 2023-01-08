f61ec54ea998a7560de64993545f463b30e934e4
Gabriel Boucher <gab.boucher91@gmail.com>
2023-01-08 12:38:09 -0500

    Added routing and components for login and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { UsersModule } from './users/users.module';
 
-const routes: Routes = [{ path: '', component: HomePageComponent }];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  {
+    path: 'user',
+    loadChildren: () => import('./users/users.module').then((m) => UsersModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { UserHomeComponent } from './user-home/user-home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserHomeComponent,
+    SignupComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     UsersRoutingModule
