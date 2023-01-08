d744d9dc5026c9441e03abf0a5310d008eec39fb
Gabriel Boucher <gab.boucher91@gmail.com>
2023-01-08 12:45:10 -0500

    Fixed the sub-routing to reuse code within the user module

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { UserHomeComponent } from './user-home/user-home.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
+  {
+    path: '',
+    component: UserHomeComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+    ],
+  },
 ];
 
 @NgModule({
