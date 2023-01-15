666948172f3d4df1a212a3d352c96603f68cec5a
Gabriel Boucher <gab.boucher91@gmail.com>
2023-01-15 11:22:45 -0500

    Finished the routing for users

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { UserHomeComponent } from './user-home/user-home.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   {
@@ -12,6 +14,8 @@ const routes: Routes = [
       { path: '', redirectTo: 'signup', pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent },
+      { path: ':id', component: UserProfileComponent },
+      { path: ':id/dashboard', component: UserDashboardComponent },
     ],
   },
 ];
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -5,13 +5,17 @@ import { UsersRoutingModule } from './users-routing.module';
 import { UserHomeComponent } from './user-home/user-home.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 
 @NgModule({
   declarations: [
     UserHomeComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    UserProfileComponent,
+    UserDashboardComponent
   ],
   imports: [
     CommonModule,
